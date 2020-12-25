b7f89578ee17c85aa0fc2440a051b8761fb299d6
yangbin <yangbinyhbn@gmail.com>
2020-12-25 13:34:14 +0800

    refine flutter analyzer

--- example/android/app/src/main/kotlin/tech/soit/example/audio_player_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package tech.soit.example.audio_player_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity() {
-}
---
+++ example/android/app/src/main/kotlin/tech/soit/flutter/audio_player_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package tech.soit.flutter.audio_player_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
