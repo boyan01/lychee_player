be41ddd15819a577c30532d97aea7be5b373178c
伯言 <yangbinyhbn@gmail.com>
2020-12-28 23:20:58 +0800

    add ffplayer

---
+++ ffplayer/ffplayer.cpp
@@ -0,0 +1,15 @@
+﻿// ffplayer.cpp: 定义应用程序的入口点。
+//
+
+#include "ffplayer.h"
+
+extern "C" {
+#include "libavutil/time.h"
+}
+
+using namespace std;
+
+int main() {
+  cout << "Hello CMake." << av_gettime_relative() << endl;
+  return 0;
+}
