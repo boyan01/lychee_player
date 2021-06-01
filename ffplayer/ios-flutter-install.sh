5b9f1c734f5f8466c042841090f52ba8bc59d781
boyan <yangbinyhbn@gmail.com>
2021-06-01 21:41:33 +0800

    build combined library for ios

--- ffplayer/ios-flutter-install.sh
+++ ffplayer/ios-flutter-install.sh
@@ -1,10 +1,14 @@
 #!/bin/zsh
 
-build_dir="build-ios"
+current_dir=$(pwd)/$(dirname "$0")
+
+build_dir="${current_dir}/build-ios"
+
 # remove cmake install prefix. which default is 'usr/local'
-cmake . -B $build_dir -G Xcode -DCMAKE_INSTALL_PREFIX="" -DCMAKE_FLUTTER_MEDIA_IOS=1 -DCMAKE_TOOLCHAIN_FILE=./ios.toolchain.cmake -DPLATFORM=OS64
+cmake "$current_dir" -B "$build_dir" -G Xcode -DCMAKE_INSTALL_PREFIX="" -DCMAKE_FLUTTER_MEDIA_IOS=1 \
+  -DCMAKE_TOOLCHAIN_FILE="${current_dir}/ios.toolchain.cmake" -DPLATFORM=OS64COMBINED
 
 # install build output to default dir.
-cd $build_dir || exit
-cmake --build . --config Release
-cmake --install . --config Release --prefix ../../ios/Library/LycheePlayer
+cd "$build_dir" || exit
+cmake --build "$build_dir" --config Release
+cmake --install "$build_dir" --config Release --prefix "${current_dir}/../ios/Library/LycheePlayer"
