484e04c19fc25ca3856cf59a9114bd52de4a729e
boyan <17426470+boyan01@users.noreply.github.com>
2022-05-15 11:35:54 +0800

    docs

--- publish.sh
+++ publish.sh
@@ -7,11 +7,11 @@ flutter pub get
 cd example
 
 cd macos && pod install && cd ..
-cd ios && pod install && cd ..
+#cd ios && pod install && cd ..
 
 cd ..
 
-./ffplayer/apple-flutter-install.sh ios all
+#./ffplayer/apple-flutter-install.sh ios all
 ./ffplayer/apple-flutter-install.sh macos
 
 flutter pub publish -n
